af182b74056a7a2e1afdd2473795ed7414cf33a4
Thomas Delos <120459971+ThomasDelosEpsi@users.noreply.github.com>
2025-09-01 14:17:01 +0200

    remove product and add calculator

--- src/main/java/com/tpre814/example/api_stock/repository/StockRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tpre814.example.api_stock.repository;
-
-import com.tpre814.example.api_stock.entity.Stock;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StockRepository extends JpaRepository<Stock, Long> {
-}
